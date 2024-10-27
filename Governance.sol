8d41d6e1a1b8b1eaea4eae69be2d1943905031a1
toadkiller24 <daotoad676@gmail.com>
2024-10-27 02:28:36 +0200

    Update ToadDaoGovernance.sol

--- Governance.sol
+++ Governance.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFractio
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
 
-contract GovernorContract is
+contract ToadDaoGovernorContract is
   Governor,
   GovernorSettings,
   GovernorCountingSimple,
